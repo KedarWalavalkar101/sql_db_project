416ba151b6273db1e2ff960d0d6c113b4a29dd1c
KedarWalavalkar101 <124864845+KedarWalavalkar101@users.noreply.github.com>
2023-07-14 20:24:15 +0530

    Update music_db_script.sql

--- music_db_script.sql
+++ music_db_script.sql
@@ -140,7 +140,7 @@ WITH popular_genre AS
 SELECT * FROM popular_genre WHERE RowNo <= 1;
 
 
-/*  Write a query that determines the customer that has spent the most on music for each country. 
+/*  Determining which customer has spent the most on music for each country. 
 Write a query that returns the country along with the top customer and how much they spent. 
 For countries where the top amount spent is shared, provide all customers who spent this amount. */
 
